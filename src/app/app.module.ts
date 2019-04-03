0380bb840b0756d67ddaf5dc03c6f28972488133
Mario Kratafila <Mario_Kratafila@epam.com>
2019-04-03 15:01:06 +0200

    login component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { WelcomeSubComponent } from './welcome-sub/welcome-sub.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    WelcomeSubComponent
+    WelcomeSubComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
