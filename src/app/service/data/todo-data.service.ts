4ca5f60552323ec0efc97b93e5fc25d313187362
Mario Kratafila <mario_kratafila@epam.com>
2019-04-05 23:46:15 +0200

    backend links updated

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -10,23 +10,23 @@ export class TodoDataService {
   constructor(private http: HttpClient) { }
 
   retrieveAllTodos(username: string) {
-    return this.http.get<Todo[]>(`http://localhost:8080/users/${username}/todos`);
+    return this.http.get<Todo[]>(`http://backend/users/${username}/todos`);
   }
 
   deleteTodo(username: string, id: any) {
-    return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
+    return this.http.delete(`http://backend/users/${username}/todos/${id}`);
   }
 
   getTodoById(username: string, id: number) {
-    return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+    return this.http.get<Todo>(`http://backend/users/${username}/todos/${id}`);
   }
 
   updateTodo(username: string, id: number, todo: Todo) {
-    return this.http.put<Todo>(`http://localhost:8080/users/${username}/todos/${id}`,todo);
+    return this.http.put<Todo>(`http://backend/users/${username}/todos/${id}`,todo);
   }
 
   createTodo(username: string, todo: Todo) {
-    return this.http.post<Todo>(`http://localhost:8080/users/${username}/todos`,todo);
+    return this.http.post<Todo>(`http://backend/users/${username}/todos`,todo);
   }
 
 
