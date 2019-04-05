3a25fee0c245d5f94c458992cc21dd1a0fb47941
Mario Kratafila <mario_kratafila@epam.com>
2019-04-06 00:46:56 +0200

    3rd attempt to fix api routes

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -10,23 +10,23 @@ export class TodoDataService {
   constructor(private http: HttpClient) { }
 
   retrieveAllTodos(username: string) {
-    return this.http.get<Todo[]>(`http://backend:8080/users/${username}/todos`);
+    return this.http.get<Todo[]>(`/api/users/${username}/todos`);
   }
 
   deleteTodo(username: string, id: any) {
-    return this.http.delete(`http://backend:8080/users/${username}/todos/${id}`);
+    return this.http.delete(`/api/users/${username}/todos/${id}`);
   }
 
   getTodoById(username: string, id: number) {
-    return this.http.get<Todo>(`http://backend:8080/users/${username}/todos/${id}`);
+    return this.http.get<Todo>(`/api/users/${username}/todos/${id}`);
   }
 
   updateTodo(username: string, id: number, todo: Todo) {
-    return this.http.put<Todo>(`http://backend:8080/users/${username}/todos/${id}`,todo);
+    return this.http.put<Todo>(`/api/users/${username}/todos/${id}`,todo);
   }
 
   createTodo(username: string, todo: Todo) {
-    return this.http.post<Todo>(`http://backend:8080/users/${username}/todos`,todo);
+    return this.http.post<Todo>(`/api/users/${username}/todos`,todo);
   }
 
 
