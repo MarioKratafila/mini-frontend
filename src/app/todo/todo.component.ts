74a8d173f7b8a801e11d97d0bbb1d596de59693b
Mario Kratafila <Mario_Kratafila@epam.com>
2019-04-05 15:10:41 +0200

    Step 63

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
+import { Todo } from '../list-todos/list-todos.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  todo: Todo;
+
+  constructor(private service : TodoDataService, private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.service.getTodoById("mario", this.id).subscribe(
+      data => this.todo = data
+    );
   }
 
 }
