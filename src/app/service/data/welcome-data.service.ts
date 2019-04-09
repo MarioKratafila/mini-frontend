d6b383bf54d5ebcdf6847902bca0175ee5f28ebd
Mario Kratafila <Mario_Kratafila@epam.com>
2019-04-09 10:42:55 +0200

    Step 83

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -11,31 +11,14 @@ export class WelcomeDataService {
 
   executeHelloWorldBeanService() {
     return this.http.get<HelloWorldBean>("http://localhost:8080/hello-world-bean");
-   // console.log("Execute Hello World Bean Service")
   }
 
   executeHelloWorldBeanServiceWithPathVariable(name) {
 
-    let headers = new HttpHeaders({
-      Authorization: this.createBasicAuthenticationHttpHeader()
-    })
+    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`);
 
-    // we need to use the "tick" character when we want to replace a variable here:
-
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`,
-    {headers : headers});
-
-   // console.log("Execute Hello World Bean Service")
-  }
-
-  createBasicAuthenticationHttpHeader() {
-    let username = 'mario';
-    let password = 'pass';
-    let basicAuthHeaderString = 'Basic '+window.btoa(username+":"+password);
-    return basicAuthHeaderString;
   }
 }
-
 export class HelloWorldBean {
   constructor(public message : string) {}
 }
--- src/app/service/http/http-intercepter-basic-auth.service.ts
+++ src/app/service/http/http-intercepter-basic-auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http';
+import { BasicAuthenticationService } from '../basic-authentication.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,18 +8,20 @@ import { HttpInterceptor } from '@angular/common/http';
 export class HttpIntercepterBasicAuthService implements HttpInterceptor {
 
   intercept(req: import("@angular/common/http").HttpRequest<any>, next: import("@angular/common/http").HttpHandler): import("rxjs").Observable<import("@angular/common/http").HttpEvent<any>> {
-    let username = 'mario';
-    let password = 'pass';
-    let basicAuthHeaderString = 'Basic '+window.btoa(username+":"+password);
     
-    req = req.clone({
-      setHeaders : {
-        Authorization : basicAuthHeaderString
-      }
-    })
+    let basicAuthHeaderString = this.basicAuthService.getAuthenticatedToken()
+    let username = this.basicAuthService.getAuthenticatedUser()
+
+    if (this.basicAuthService && username) {
+      req = req.clone({
+        setHeaders : {
+          Authorization : basicAuthHeaderString
+        }
+      })
+    }
 
     return next.handle(req);
   }
 
-  constructor() { }
+  constructor(private basicAuthService : BasicAuthenticationService) { }
 }
