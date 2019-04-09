b8c103db38191bb472f8a5db5b463b343ede1570
Mario Kratafila <Mario_Kratafila@epam.com>
2019-04-09 16:32:10 +0200

    fixed the hardcoded username of list todos ts

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoDataService } from '../service/data/todo-data.service';
 import { Router } from '@angular/router';
+import { BasicAuthenticationService } from '../service/basic-authentication.service';
 
 @Component({
   selector: 'app-list-todos',
@@ -12,14 +13,14 @@ export class ListTodosComponent implements OnInit {
   todos : Todo[];
   message: string;
 
-  constructor(private todoDataService : TodoDataService, private router: Router) { }
+  constructor(private todoDataService : TodoDataService, private router: Router, private authService : BasicAuthenticationService) { }
 
   ngOnInit() {
     this.refreshTodos();
   }
 
   deleteTodo(id) {
-    this.todoDataService.deleteTodo('mario', id).subscribe(
+    this.todoDataService.deleteTodo(this.authService.getAuthenticatedUser(), id).subscribe(
       response => {
         console.log(response);
         this.message = `Todo with id ${id} deleted successfully!`;
@@ -40,7 +41,7 @@ export class ListTodosComponent implements OnInit {
   }
 
   refreshTodos() {
-    this.todoDataService.retrieveAllTodos('mario').subscribe(
+    this.todoDataService.retrieveAllTodos(this.authService.getAuthenticatedUser()).subscribe(
       response => {this.todos=response},
       error => console.log(error.error.message)
     );
