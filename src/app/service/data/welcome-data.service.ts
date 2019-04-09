9f6cdb125b890f7e6cfe806cbdc0c961a167a079
Mario Kratafila <Mario_Kratafila@epam.com>
2019-04-09 11:38:54 +0200

    Step 84

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { API_URL } from 'src/app/app.constants';
 
 
 @Injectable({
@@ -10,12 +11,12 @@ export class WelcomeDataService {
   constructor(private http: HttpClient) { }
 
   executeHelloWorldBeanService() {
-    return this.http.get<HelloWorldBean>("http://localhost:8080/hello-world-bean");
+    return this.http.get<HelloWorldBean>(`${API_URL}/hello-world-bean`);
   }
 
   executeHelloWorldBeanServiceWithPathVariable(name) {
 
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`);
+    return this.http.get<HelloWorldBean>(`${API_URL}/hello-world/path-variable/${name}`);
 
   }
 }
